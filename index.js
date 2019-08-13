50ff1559583a7e2d93b740c63c315feef5259bf5
Ben Davenport <benjamin.p.davenport@gmail.com>
2019-08-13 11:19:13 -0400

    Pulling nerd content. 2 names at random

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
-// Without "./", Node looks
-// in its global module folder
 const http = require("http");
-function handleRequestAndResponse(req, res) {
-    console.log(req.url);
-    const timestamp = new Date();
-    console.log("Received a Request");
-    res.end(`<h1>Hello World ${timestamp}</h1>`);  // .end() sends the response.
-                                          // It "tapes up the box and ships it".
-}
-// What's a server??!?
-// A server is a piece of code
-// or a computer running a piece of code
-// that does something useful for YOU.
-// Usually, servers store
-// data and often manipulate that data.
-// const server = http.createServer(handleRequestAndResponse);
+const nerds = require('nerds');
+// function handleRequestAndResponse(req, res) {
+//     console.log(req.url);
+//     const timestamp = new Date();
+//     console.log("Received a Request");
+//     res.end(`<h1>Hello${timestamp}</h1>`);  // .end() sends the response.
+//                                           // It "tapes up the box and ships it".
+// }
+// [getRandomInt(0, nerdArray.length-1)]
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min}
 const server = http.createServer((req, res) => {
-    console.log(req.url);
-    const timestamp = new Date();
-    console.log("I totally received a Request!!!!!!!!!!!!!!!!!!!");
-    res.end(`<h1>Hello World ${timestamp}</h1>`);  // .end() sends the response.
-                                          // It "tapes up the box and ships it".
+    const nerdArray = ["Harry Potter", "Pokemon", "Star Wars"];
+    randomInt = getRandomInt(0, nerdArray.length)
+    // const character = nerds.resolve(nerdArray[randomInt(0,nerdArray.length)]).asArray()[0];
+    const character = nerds.resolve(nerdArray[randomInt], 3).asArray();
+    const character1 = character[0]
+    const character2 = character[1]
+    // const character2 = nerds.resolve(nerdArray[randomInt], 3).asArray()[1];
+    // const character3 = nerds.resolve(nerdArray[randomInt], 3).asArray()[2];
+
+    nerdArray[randomInt] === "Harry Potter"? res.end(`Hello, ${character1.first} ${character1.last} and ${character2.first} ${character2.last} from ${nerdArray[randomInt]}`): res.end(`Hello, ${character1.name} and ${character2.name} from ${nerdArray[randomInt]}`);
 });
 
-// http.createServer() accepts a function and returns a "server"
-// When the server receives a Request:
-// 1. It puts together *most* of a Response.
-// 2. It calls *your* function and passes it the Request and Response
-
-
-// What is 3000?
 server.listen(3000);
